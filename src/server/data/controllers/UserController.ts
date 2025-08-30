070020132496bd68e62ebcaa6cddb4ec1ee3f673
ericpapazzz <ericrendong@gmail.com>
2025-08-30 13:15:36 -0700

    feat: Add User Controller

---
+++ src/server/data/controllers/UserController.ts
@@ -0,0 +1,117 @@
+import { type Request, type Response } from "express";
+import { User } from "../models/User.js";
+import { UserRepository } from "../repositories/UserRepository.js";
+
+/**
+ * userController class handles HTTP requests related to user operations
+ * provides CRUD operations for user management
+ */
+class UserController {
+    // creates a new user
+    async create(req: Request, res: Response) {
+        try {
+            const newUser = new User();
+
+            newUser.username = req.body.username;
+
+            await new UserRepository().save(newUser);
+
+            res.status(200).json({
+                status: "OK",
+                message: "Successfully created User.",
+                data: newUser
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "Internal Server Error!",
+                message: "Internal Server Error!",
+            })
+        }
+    }
+
+    // updates an existing user
+
+    async update(req: Request, res: Response) {
+        try {
+            let id = parseInt(req.params["id"] || "0");
+            
+            const updatedUser = new User();
+
+            updatedUser.user_id = id
+            updatedUser.username = req.body.username;
+
+            await new UserRepository().update(updatedUser);
+
+            res.status(200).json({
+                status: "OK",
+                message: "Successfully updated User.",
+                data: updatedUser
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "Internal Server Error!",
+                message: "Internal Server Error!",
+            })
+        }
+    }
+
+//    deletes a user by ID
+    async delete(req: Request, res: Response) {
+        try {
+            let id = parseInt(req.params["id"] || "0");
+            
+            await new UserRepository().delete(id)
+
+            res.status(200).json({
+                status: "OK",
+                message: "Successfully deleted User.",
+                data: { deletedId: id }
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "Internal Server Error!",
+                message: "Internal Server Error!",
+            })
+        }
+    }
+
+    // retrieves a user by their ID
+    async findById(req: Request, res: Response) {
+        try {
+            let id = parseInt(req.params["id"] || "0");
+            
+            const user = await new UserRepository().retrieveById(id);
+
+            res.status(200).json({
+                status: "OK",
+                message: "Successfully fetched user by id!",
+                data: user,
+            });
+        } catch (err) {
+            res.status(500).json({
+                status: "Internal Server Error!",
+                message: "Internal Server Error!",
+            });
+        }
+    }
+
+    // retrieves all users from the database
+    async findAll(req: Request, res: Response) {
+        try {
+            const users = await new UserRepository().retrieveAll();
+
+            res.status(200).json({
+                status: "OK",
+                message: "Successfully fetched all users!",
+                data: users,
+            });
+        } catch (err) {
+            res.status(500).json({
+                status: "Internal Server Error!",
+                message: "Internal Server Error!",
+            });
+        }
+    }
+}
+
+export default new UserController()
